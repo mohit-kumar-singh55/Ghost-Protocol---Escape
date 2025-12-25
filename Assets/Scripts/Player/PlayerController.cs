8a7f9c6cbe3760049a91267c29d6662e732176f8
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2025-12-25 17:55:02 +0900

    Update: added normal jump & wall jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,11 +9,19 @@ enum Rotation { Right = 0, Left = 180 }
 public class PlayerController : MonoBehaviour
 {
     #region Serialized Fields
-    [Header("Settings")]
+    [Header("Move Settings")]
     [SerializeField] private float _speed = 10f;
     [SerializeField] private float _dashForce = 10f;
     [Tooltip("Time to wait before next dash")]
     [SerializeField] private float _dashCooldownTime = 3f;
+
+    [Space(10)]
+    [Header("Jump Settings")]
+    [SerializeField] private float _jumpForce = 8f;     // normal jump force
+    [SerializeField] private float _wallJumpForce = 12f;     // wall jump force
+    [SerializeField] private float _gravityScaleWhenFalling = 4f;   // increase gravity when in air
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private LayerMask _wallLayer;
 
     [Space(10)]
     [Header("References")]
@@ -26,11 +34,15 @@ public class PlayerController : MonoBehaviour
     private CrosshairController _crosshair;
     private SpriteSkin _spriteSkin;
     private Rotation _playerCurrentRotation = Rotation.Right;
-    private Transform _spine;
-    private Quaternion _originalSpineRotation;
+    private Transform _spine;   // sprites with shooting animation are rigged, so we can rotate the spine bone
     private float _moveX;
     private float _dashTimer = 0;
-    private bool _isShooting;
+    private bool _isShooting = false;
+    private bool _isJumping = false;
+    private bool _isGrounded = true;
+    private bool _isWall = false;
+    private Vector3 _wallTouchPoint;
+    private float _originalGravityScale;
 
     // animator params
     private readonly int Speed = Animator.StringToHash("Speed");
@@ -41,6 +53,7 @@ public class PlayerController : MonoBehaviour
     public static event Action<bool> OnPlayerShoot = delegate { };
     #endregion
 
+    #region Unity Methods
     void Awake()
     {
         // get references
@@ -50,7 +63,9 @@ public class PlayerController : MonoBehaviour
 
         // get spine
         _spine = _spriteSkin.rootBone.GetChild(0);
-        _originalSpineRotation = _spine.rotation;
+
+        // original gravity scale
+        _originalGravityScale = _rb.gravityScale;
     }
 
     void Start()
@@ -60,6 +75,7 @@ public class PlayerController : MonoBehaviour
         if (!_crosshair || !_gun)
         {
             Debug.LogError("Crosshair not found or Gun is not assigned!");
+            enabled = false;
             return;
         }
     }
@@ -75,11 +91,9 @@ public class PlayerController : MonoBehaviour
         // update player rotation based on crosshair if player is shooting
         if (_isShooting)
         {
-            HandleVerticalRotationByCrosshair();
+            HandleHorizontalRotationByCrosshair();
             HandleUpperBodyRotationByCrosshair();
         }
-        // ? if needed,reset spine rotation if not shooting
-        // else ResetUpperBodyRotation();
     }
 
     void FixedUpdate()
@@ -88,6 +102,25 @@ public class PlayerController : MonoBehaviour
         HandleMove();
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // ground check
+        if (IsInLayerMask(collision.gameObject.layer, _groundLayer))
+        {
+            _isGrounded = true;
+            _isJumping = false;
+            _rb.gravityScale = _originalGravityScale;       // reset gravity scale
+        }
+
+        // wall check
+        if (IsInLayerMask(collision.gameObject.layer, _wallLayer))
+        {
+            _isWall = true;
+            _wallTouchPoint = collision.ClosestPoint(transform.position);
+        }
+    }
+    #endregion
+
     #region Input System Callbacks
     private void OnMove(InputValue val)
     {
@@ -95,13 +128,15 @@ public class PlayerController : MonoBehaviour
 
         // flip sprite, only if the player is moving and not shooting
         if (_moveX == 0 && !_isShooting) return;
-        _playerCurrentRotation = _moveX < 0 ? Rotation.Left : Rotation.Right;
-        transform.rotation = Quaternion.Euler(0, (float)_playerCurrentRotation, 0);
+        FlipPlayerHorizontally(_moveX < 0);
     }
 
     private void OnShoot(InputValue val)
     {
         ToogleShoot(val.isPressed);
+
+        // reset player rotation in the direction of movement when stop shooting
+        if (!val.isPressed) FlipPlayerHorizontally(_moveX < 0);
     }
 
     // 短い間の急ダッシュ
@@ -118,8 +153,37 @@ public class PlayerController : MonoBehaviour
         // sudden force to dash
         _rb.AddForceX(_moveX * _dashForce, ForceMode2D.Impulse);
     }
+
+    private void OnJump(InputValue val)
+    {
+        // ** wall jump **
+        if (_isJumping && _isWall && !_isGrounded)
+        {
+            // check for side wall jump
+            Vector3 dirToJump = (transform.position - _wallTouchPoint).normalized + Vector3.up;
+            dirToJump.z = 0;
+            dirToJump.Normalize();
+
+            // Debug.DrawRay(transform.position, dirToJump, Color.red, 0f, true);
+
+            _rb.AddForce(dirToJump * _wallJumpForce, ForceMode2D.Impulse);
+
+            _isWall = false;
+            return;
+        }
+
+        // will do normal jump only if grounded
+        if (!_isGrounded) return;
+
+        // ** normal jump **
+        _isJumping = true;
+        _isGrounded = false;
+        _rb.gravityScale = _gravityScaleWhenFalling;        // increase gravity when in air
+        _rb.AddForceY(_jumpForce, ForceMode2D.Impulse);
+    }
     #endregion
 
+    #region Private Methods
     private void HandleMove()
     {
         float targetVelocity = _moveX * _speed;
@@ -138,7 +202,14 @@ public class PlayerController : MonoBehaviour
         OnPlayerShoot?.Invoke(_isShooting);
     }
 
-    private void HandleVerticalRotationByCrosshair()
+    private void FlipPlayerHorizontally(bool flipLeft)
+    {
+        // flip player
+        _playerCurrentRotation = flipLeft ? Rotation.Left : Rotation.Right;
+        transform.rotation = Quaternion.Euler(0, (float)_playerCurrentRotation, 0);
+    }
+
+    private void HandleHorizontalRotationByCrosshair()
     {
         // get directions
         Vector3 playerRight = transform.right.normalized;
@@ -151,13 +222,12 @@ public class PlayerController : MonoBehaviour
         if (!isCrosshairBehind) return;
 
         // change rotation
-        _playerCurrentRotation = _playerCurrentRotation == Rotation.Right ? Rotation.Left : Rotation.Right;
-        transform.rotation = Quaternion.Euler(0, (float)_playerCurrentRotation, 0);
+        FlipPlayerHorizontally(_playerCurrentRotation == Rotation.Right);
     }
 
     private void HandleUpperBodyRotationByCrosshair()
     {
-        // get spine transform
+        // get direction from spine to crosshair
         Vector3 dir = (_crosshair.transform.position - _spine.position).normalized;
 
         // get the angle between right vector and direction to crosshair
@@ -168,6 +238,7 @@ public class PlayerController : MonoBehaviour
         _spine.rotation = Quaternion.Euler(_spine.rotation.eulerAngles.x, _spine.rotation.eulerAngles.y, zAngle);
     }
 
-    // reset spine rotation to original
-    private void ResetUpperBodyRotation() => _spine.rotation = _originalSpineRotation;
+    // check if a layer is in a layermask
+    private bool IsInLayerMask(int layer, LayerMask layerMask) => ((1 << layer) & layerMask.value) != 0;
+    #endregion
 }
